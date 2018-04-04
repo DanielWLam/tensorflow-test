caa521b6b01a7472c777336d19383e1ff0e1de0f
阿定 <wending.lwd@alibaba-inc.com>
2018-04-04 10:34:29 +0800

    init

---
+++ a.js
@@ -0,0 +1,20 @@
+const tf = require('@tensorflow/tfjs')
+
+const model = tf.sequential();
+model.add(tf.layers.dense({
+  units: 1,
+  inputShape: [1]
+}))
+
+model.compile({
+  loss: 'meanSquaredError',
+  optimizer: 'sgd'
+})
+
+const xs = tf.tensor2d([1, 2, 3, 4], [4, 1])
+const yx = tf.tensor2d([1, 3, 5, 7], [4, 1])
+
+model.fit(xs, yx)
+.then(() => {
+  model.predict(tf.tensor2d([5], [1, 1])).print()
+})
